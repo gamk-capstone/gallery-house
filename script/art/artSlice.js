9e4d45c80b7391ff6131bd52ca87b67d08eb45b8
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-16 14:41:19 -0400

    listing_ids call working

--- script/art/artSlice.js
+++ script/art/artSlice.js
@@ -7,12 +7,16 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchAllArtAsync = createAsyncThunk("artFetchAll", async () => {
   try {
+    //GETs 100 listing_ids
     let { data } = await axios.get(
-      `https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v3/application/listings/active?limit=1000&api_key=5wdviq8lfzumur7vsxlc7i3g`,
+      `https://cors-anywhere.herokuapp.com/https://api.etsy.com/v3/application/listings/active?limit=100&keywords=art,artwork,paintings&api_key=5wdviq8lfzumur7vsxlc7i3g`,
       { headers: { "x-api-key": "5wdviq8lfzumur7vsxlc7i3g" } }
     );
-
-    console.log(data)
+    console.log("data", data)
+    const listing_ids = data.results.map((l) => l.listing_id)
+    const image_ids = data.results.map((l) => l.image_ids)
+    console.log(listing_ids)
+    console.log(image_ids)
     return data;
   } catch (error) {
     console.log(error);
