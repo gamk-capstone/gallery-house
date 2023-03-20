15f5d7a0d8d5d4b07179997dc3f878ec5eda8fb6
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-20 17:59:48 -0400

    initial set up is working correctly

--- client/components/galleryWall/FiveImgGalleryWall.js
+++ client/components/galleryWall/FiveImgGalleryWall.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const FiveImageGalleryWall = () => {
   return (
-    <div className="FiveImageWallParentDiv">
-      <div className="FiveImageWallTop">
+    <div className="flex justify-center flex-col gap-1">
+      <div className="flex justify-center flex-row items-end gap-1">
         <img src="/white.jpeg" className="landscapeRectangle" />
         <img src="/white.jpeg" className="square" />
       </div>
