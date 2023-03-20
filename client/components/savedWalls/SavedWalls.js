20a7c5f6ed13a5eebfa3c79c36130738d21d925b
Malinda Singh <malinda.singh@gmail.com>
2023-03-20 15:34:47 -0500

    deleted savedwalls

--- client/components/savedWalls/SavedWalls.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
-const GalleryWall = (props) => {
-  const username = useSelector((state) => state.auth.me.username);
-  return <div>{username}'s Saved Galleries! </div>;
-};
-
-export default GalleryWall;
