68a90dae4a5ce67662002118a9e60a0bb95b6031
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-21 17:27:04 -0400

    Navbar documented

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -3,13 +3,23 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../app/store";
 
+/**
+ * `Navbar` component
+ * @returns the correct Link options based on whether a user is logged in or not.
+ */
+
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  /**
+   * `logoutAndRedirectHome` dispatches a logout action (removing a user's token from local storage) and navigates a user to the
+   * Gallery House landing page
+   */
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate("/login");
+    navigate("/home");
   };
 
   return (
@@ -17,7 +27,7 @@ const Navbar = () => {
       <nav>
         {isLoggedIn ? (
           <div className="flex flex-row justify-center gap-4">
-            {/* The navbar will show these links after you log in */}
+            {/* The navbar will show these links after a user logs in */}
             <Link to="/home">Home</Link>
             <Link to="/create">Create</Link>
             <Link to="/saved">Saved Galleries</Link>
@@ -27,7 +37,7 @@ const Navbar = () => {
           </div>
         ) : (
           <div className="flex flex-row justify-center gap-4">
-            {/* The navbar will show these links before you log in */}
+            {/* The navbar will show these links before a user logs in and when a user logs out */}
             <Link to="/home">Gallery House</Link>
             <Link to="/create">Create</Link>
             <Link to="/login">Login</Link>
