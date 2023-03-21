906b503df6a3daa7432236e16f5382bdc47f49a4
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-21 18:09:13 -0400

    api/index commented

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,16 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
-router.use('/art', require("./art"));
-router.use('/user', require("./user"));
+/**
+ * This is like a table of contents for our routers.
+ * Instead of having all our express routes here, we've separated them out into different files, and required the routers here.
+ */
+router.use("/users", require("./users"));
+router.use("/art", require("./art"));
+router.use("/user", require("./user"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
