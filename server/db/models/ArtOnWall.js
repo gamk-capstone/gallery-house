4fd230f02dd13bbd83bd11c366a1ae62be442082
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-21 18:22:03 -0400

    ArtOnWall documented

--- server/db/models/ArtOnWall.js
+++ server/db/models/ArtOnWall.js
@@ -1,6 +1,10 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 
+/**
+ * `ArtOnWall` model is created by Advanced M:N Associations.
+ * https://sequelize.org/docs/v6/advanced-association-concepts/advanced-many-to-many/
+ */
 const ArtOnWall = db.define("artOnWall", {});
 
 module.exports = ArtOnWall;
