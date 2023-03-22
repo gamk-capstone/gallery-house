1ce480291db7f691eae5797fb4a9f532eabbd8e9
Katrina <katrossi414@gmail.com>
2023-03-22 15:42:47 -0500

    galleryWall slice created

--- client/components/galleryWall/galleryWallSlice.js
+++ client/components/galleryWall/galleryWallSlice.js
@@ -1,8 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const updateEmptyFrameCount = createAsyncThunk("updateEmptyFrameCount", async (count) => {
+export const fetchEtsyImages = createAsyncThunk("updateEmptyFrameCount", async (searchParams) => {
   try {
-    const { data } = await axios.put(`/api/`)
+    const { data } = await axios.get(`/api/etsyArt/${searchParams.hueNum}`, 
+    searchParams
+    );
+    return data;
+  } catch (err) {
+    console.error(err);
   }
-})
+});
+
+export const galleryWallSlice = createSlice({
+  name: "galleryWall",
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchEtsyImages.fulfilled, (state, action) => {
+      return action.payload
+    });
+  }
+});
+
+export const selectGalleryWall = (state) => {
+  return state.galleryWall;
+};
+
+export default galleryWallSlice.reducer;
