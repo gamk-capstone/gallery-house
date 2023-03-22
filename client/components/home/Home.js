36df440c37c57a299b0d2581933ea6eb88517253
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-22 19:22:57 -0400

    Initial idea for Landing Page

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -1,13 +1,28 @@
 import React from "react";
-
+import FiveImgGalleryWall from "../galleryWall/FiveImgGalleryWall";
 /**
- * `Home` component 
+ * `Home` component
  * @returns HTML for the Gallery House landing page
  */
 const Home = () => {
-    return (
-        <div>GALLERY HOUSE LANDING PAGE</div>
-    )
+  return (
+    <div className="grid grid-rows-1 justify-items-center">
+      <div className="font-house-font text-3xl">
+        Gallery{" "}
+        <span className="font-house-font font-light text-3xl text-pink-700">
+          ·
+        </span>{" "}
+        House
+      </div>
+      <div className="grid grid-rows-2 justify-items-center">
+        <FiveImgGalleryWall className="row-start-1 row-span-1"></FiveImgGalleryWall>
+        <img
+          src="/sofa-beige-rounded.png"
+          className="row-start-2 row-span-1 max-w-[900px]"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
