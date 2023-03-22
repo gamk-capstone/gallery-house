bb47945d63950e563d987b1b2fb0f826d9b32812
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-22 16:29:58 -0400

    Create route removed

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -54,8 +54,6 @@ const AppRoutes = () => {
             path="/signup"
             element={<AuthForm name="signup" displayName="Sign Up" />}
           />
-          {/* The /create route brings a user to the `AuthForm` component (method signup) */}
-          <Route path="/create" element={<GalleryWall />} />
         </Routes>
       )}
     </div>
