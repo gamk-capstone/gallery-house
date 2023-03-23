2a72813691c295686b0f39fa8ba11166ada6b767
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-23 17:21:05 -0400

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
-
 module.exports = {
   content: ["./client/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'house-font': ['Le Havre Black', 'sans-serif'],
-      }
-    }
+        "house-font": ["Le Havre Black", "sans-serif"],
+        colors: {
+          pink: "#fa67ce",
+          "dark-pink": "#ad368a",
+          teal: "#6de2d4",
+          "dark-teal": "#71ada8",
+          black: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
 };
