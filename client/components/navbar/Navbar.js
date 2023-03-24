73f12d9317a15c10515c3c4427a2f8a3e6d578a1
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-24 11:00:09 -0400

    navbar styling in progress. Need to add border

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -26,21 +26,53 @@ const Navbar = () => {
     <div>
       <nav>
         {isLoggedIn ? (
-          <div className="flex flex-row justify-center gap-4">
-            {/* The navbar will show these links after a user logs in */}
-            <Link to="/home">Home</Link>
-            <Link to="/create">Create</Link>
-            <Link to="/saved">Saved Galleries</Link>
-            <button type="button" onClick={logoutAndRedirectHome}>
+          <div className="flex flex-row justify-between items-center pr-16">
+            <div className="flex flex-row justify-start items-center gap-32 p-16">
+              <img
+                src="/gallery-house.png"
+                alt="Gallery House logo"
+                className="max-w-[20rem]"
+              />
+              <div className="flex flex-row gap-8">
+                {/* The navbar will show these links after a user logs in */}
+                <Link to="/home" className="nav-link">
+                  Home
+                </Link>
+                <Link to="/create" className="nav-link">
+                  Create
+                </Link>
+                <Link to="/saved" className="nav-link">
+                  Saved Galleries
+                </Link>
+              </div>
+            </div>
+            <button
+              type="button"
+              onClick={logoutAndRedirectHome}
+              className="text-black font-house-bold text-3xl focus:text-pink hover:text-pink rounded-full border-4 border-solid border-pink hover:text-pink hover:border-black px-4 py-2"
+            >
               Logout
             </button>
           </div>
         ) : (
-          <div className="flex flex-row justify-center gap-4">
-            {/* The navbar will show these links before a user logs in and when a user logs out */}
-            <Link to="/home">Gallery House</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+          <div className="flex flex-row justify-start items-center gap-32 p-16">
+            <img
+              src="/gallery-house.png"
+              alt="Gallery House logo"
+              className="max-w-[20rem]"
+            />
+            <div className="flex flex-row gap-8">
+              {/* The navbar will show these links before a user logs in and when a user logs out */}
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+              <Link to="/login" className="nav-link">
+                Login
+              </Link>
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+            </div>
           </div>
         )}
       </nav>
