f5b7ba5f25d626155b6c8754f44f468f3bcc1f75
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-26 10:48:56 -0400

    walls/fetchAll thunk returning results of /api/user/:userId/walls

--- client/components/galleryWall/galleryWallSlice.js
+++ client/components/galleryWall/galleryWallSlice.js
@@ -18,6 +18,23 @@ export const fetchEtsyImages = createAsyncThunk(
   }
 );
 
+
+/**
+ * `getWallsAysnc` GETS data at /api/user/:userId/walls and returns all walls for this user
+ */
+export const fetchWallsAsync = createAsyncThunk(
+  "walls/fetchAll",
+  async (userId) => {
+    try {
+      const { data } = await axios.get(`http://localhost:8080/api/user/${userId}/walls`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+
 /**
  * `saveWallAsync` POSTS data at /api/walls
  */
@@ -39,6 +56,7 @@ export const galleryWallSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchEtsyImages.fulfilled, (state, { payload }) => payload);
+    builder.addCase(fetchWallsAsync.fulfilled, (state, {payload}) => payload);
     builder.addCase(saveWallAsync.fulfilled, (state, { payload }) => {
       // state.push(payload);
       payload;
--- client/components/savedWall/SavedWall.js
+++ client/components/savedWall/SavedWall.js
@@ -1,12 +1,39 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  fetchWallsAsync,
+  selectGalleryWall,
+} from "../galleryWall/galleryWallSlice";
 
 const SavedWall = (props) => {
   const username = useSelector((state) => state.auth.me.username);
+  const { id } = useSelector((state) => state.auth.me);
+  const walls = useSelector(selectGalleryWall);
+  const dispatch = useDispatch();
+
+  // const [savedWalls, setSavedWalls] = useState([]);
+
+  useEffect(() => {
+    dispatch(fetchWallsAsync(id));
+    // setSavedWalls(getSavedWals);
+  }, [dispatch]);
 
   return (
     <div className="galleryWallParentDiv">
-      <div>{username}'s Saved Galleries! </div>;
+      <div>{username}'s Saved Galleries! </div>
+      {walls ? (
+        walls.map((wall) => {
+          <div>{wall.id}</div>;
+        })
+      ) : (
+        <div>
+          You don't have any galleries yet. Start{" "}
+          <span>
+            <Link to="/create">creating!</Link>
+          </span>
+        </div>
+      )}
     </div>
   );
 };
