fad381d6a6ce9bcfc5c8f3e75703e7f44b49b432
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-27 18:08:57 -0400

    single CSS file is working

--- client/components/auth/AuthForm.js
+++ client/components/auth/AuthForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { authenticate } from "../../app/store";
 import { Link } from "react-router-dom";
-import "./auth.css";
+// import "./auth.css";
 
 /**
  * The `AuthForm` component can be used for Login or Sign Up.
