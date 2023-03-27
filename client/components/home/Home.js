b1c6adeb9c6208ff12c35548b22d5af1a64d1f86
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-27 11:29:03 -0400

    homepage intitial styling complete

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -12,18 +12,18 @@ const Home = () => {
 
   return (
     <div className="flex flex-col items-center pt-[3rem]">
-      <div className="flex flex-col bg-yellow w-[60%] p-40">
-        <h1 className="font-logo-font text-dark-pink text-7xl">
+      <div className="flex flex-col items-center gap-6 bg-yellow w-[80%] p-40">
+        <h1 className="font-logo-font text-dark-pink text-7xl w-[90%]">
           So much color, so little time.
         </h1>
-        <p className="font-house-regular-italic text-dark-pink text-2xl">
-          We match your art to complimentary pieces from Esty sellers so that
-          you can seamlessly create beautiful custom gallery walls informed by
-          color theory
+        <p className="font-house-regular-italic text-dark-pink text-2xl w-[90%]">
+          We match your art to complimentary pieces from Esty sellers, allowing
+          you to seamlessly create beautiful custom gallery walls informed by
+          color theory.
         </p>
         <button
           onClick={navigateToLogin}
-          className="py-2 px-4 text-dark-pink font-house-regular text-xl rounded-full border-4 border-dark-pink hover:border-pink hover:text-pink"
+          className="py-2 px-4 text-dark-pink font-house-regular text-2xl max-w-[10rem] rounded-full border-4 border-dark-pink hover:border-black hover:text-black"
         >
           Start Creating
         </button>
