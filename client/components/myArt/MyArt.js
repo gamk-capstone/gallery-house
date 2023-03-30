ce3b4529d77bcf4b1956a78a2139eab24df5b313
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-30 12:27:25 -0400

    myArt styled

--- client/components/myArt/MyArt.js
+++ client/components/myArt/MyArt.js
@@ -5,6 +5,7 @@ import {
   selectUserArt,
   deleteUserArtAsync,
 } from "../userArt/allUsersArtSlice";
+import styles from "../styles/MyArt.module.css";
 
 const MyArt = ({ setImageUrl, setCompColor }) => {
   const username = useSelector((state) => state.auth.me.username);
@@ -22,11 +23,15 @@ const MyArt = ({ setImageUrl, setCompColor }) => {
 
   const RenderArt = () => {
     if (art.length === 0) {
-      return <h2>You haven't uploaded any art yet.</h2>;
+      return (
+        <h2 className={styles.myArtH2}>You haven't uploaded any art yet.</h2>
+      );
     }
     return (
       <div>
-        <h2>You have {art.length} piece(s) of art.</h2>
+        <h2 className={styles.myArtH2}>
+          You have {art.length} piece(s) of art.
+        </h2>
         <div>
           {art?.map((piece) => {
             return (
@@ -49,8 +54,9 @@ const MyArt = ({ setImageUrl, setCompColor }) => {
     );
   };
   return (
-    <div className="myArt-container">
-      <h1>{`${username}'s Art`}</h1>
+    <div className={styles.myArtContainer}>
+      <h1 className={styles.myArtH1}>{`${username}'s Art`}</h1>
+      <hr className={styles.hr} />
       <RenderArt />
     </div>
   );
