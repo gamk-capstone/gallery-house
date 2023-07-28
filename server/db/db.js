99d07ef57271ed744ecc38d06a54304a933a69f6
Alexandra Bunty <buntyalexandra@gmail.com>
2023-07-28 11:33:40 -0400

    got server running locally but now getting auth errors

--- server/db/db.js
+++ server/db/db.js
@@ -1,33 +1,35 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
-require("dotenv").config()
+const Sequelize = require("sequelize");
+const pkg = require("../../package.json");
+require("dotenv").config();
 
 // databaseName used for testing on local machine
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
 const config = {
   logging: false,
 };
 
-if(process.env.LOGGING === 'true'){
-  delete config.logging
+if (process.env.LOGGING === "true") {
+  delete config.logging;
 }
 
-https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if(process.env.DATABASE_URL){
+//stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+https: if (process.env.DATABASE_URL) {
   config.dialectOptions = {
     ssl: {
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   };
 }
 
 const db = new Sequelize(
-  // `postgres://localhost:5432/${databaseName}?sslmode=disable`, //comment out this line to connect to db hosted on bit.io
-  // || 
-  process.env.DATABASE_URL, //comment out this link to conneect to postgres db on your local machine
-  config)
-module.exports = db
+  `postgres://localhost:5432/${databaseName}?sslmode=disable`, //comment out this line to connect to db hosted on bit.io
+  // ||
+  // process.env.DATABASE_URL, //comment out this link to conneect to postgres db on your local machine
+  config
+);
+module.exports = db;
 
 // Our url for connected to postgres on our local machine
 // `postgres://localhost:5432/${databaseName}`,
